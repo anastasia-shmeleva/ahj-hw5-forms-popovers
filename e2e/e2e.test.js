e7e5bd9894afbae01f42357de8e9007d0b6b49d2
Anastasia Shmeleva <anastasiashmeleva@Anastasias-MacBook-Air-2.local>
2021-09-08 21:02:10 +0300

    server fix

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,23 +1,36 @@
 import puppetteer from 'puppeteer';
+import { fork } from 'child_process';
 
 jest.setTimeout(30000); // default puppeteer timeout
 
 describe('should toggle popup on click', () => {
   let browser = null;
   let page = null;
-  const baseUrl = 'http://localhost:8080';
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
 
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
     browser = await puppetteer.launch({
-      // headless: false, // show gui
-      // slowMo: 500,
-      // devtools: true, // show devTools
+      headless: false, // show gui
+      slowMo: 250,
+      devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
 
   afterAll(async () => {
     await browser.close();
+    server.kill();
   });
 
   test('should toggle popup', async () => {
